26927c30f2ad0114d5e3884fc8605bef8adbb98b
Coalery <doralife12@gmail.com>
2025-05-21 19:08:40 +0900

    feat: 부하 시나리오 구현

--- load-test/script.js
+++ load-test/script.js
@@ -1,6 +1,14 @@
 import http from "k6/http";
 import { check } from "k6";
 
+export const options = {
+  stages: [
+    { duration: "1s", target: 50 },
+    { duration: "1m", target: 50 },
+    { duration: "1s", target: 0 },
+  ],
+};
+
 export default function () {
   const host = __ENV.BASE_URL;
   if (!host) {
@@ -8,7 +16,7 @@ export default function () {
     return;
   }
 
-  const url = `${host}`;
+  const url = `${host}/load`;
   const res = http.get(url);
 
   check(res, { "status is 200": (r) => r.status === 200 });
